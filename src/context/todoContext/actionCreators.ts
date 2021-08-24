73c3be8349fd5a2aec1327798402d3b1485e371c
sanam <simian114@gmail.com>
2021-08-25 01:35:55 +0900

    [#23] feat: update 구현

--- src/context/todoContext/actionCreators.ts
+++ src/context/todoContext/actionCreators.ts
@@ -1,10 +1,18 @@
-import { CREATE, LOAD, REMOVE } from "./actionTypes";
+import { CREATE, LOAD, REMOVE, UPDATE } from "./actionTypes";
 import { Action, CreateTodoDto } from "types/context";
 
 export const create = (createTodoDto: CreateTodoDto): Action => {
   const payload = createTodoDto;
   return { type: CREATE, payload }
 }
+
 export const load = (): Action => ({type: LOAD})
 
 export const remove = (id: number): Action => ({type: REMOVE, payload: id})
+
+export const update = (payload: {}): Action => {
+  return {
+    type: UPDATE,
+    payload
+  }
+}
--- src/context/todoContext/reducer.ts
+++ src/context/todoContext/reducer.ts
@@ -1,8 +1,9 @@
 import { LSHelper } from 'utils';
-import { CREATE, REMOVE, LOAD } from './actionTypes';
+import { CREATE, REMOVE, LOAD, UPDATE } from './actionTypes';
 import { IState, Action, CreateTodoDto} from 'types/context';
 import { ITodo, TPriority, TStatus } from 'types/todo';
 import { TODOS } from 'utils/contants';
+import { ITodos } from 'types';
 
 export default function reducer(state: IState, action: Action): IState {
   const { type, payload } = action;
@@ -15,9 +16,10 @@ export default function reducer(state: IState, action: Action): IState {
         todos : state.todos.concat(newTodo),
         nextId: newTodo.id + 1,
       }
+    case UPDATE:
+      return { ...state, todos: updateTodos(payload, state.todos) }
     case REMOVE:
-      const todos = state.todos.filter((todo) => todo.id !== payload );
-      return { ...state, todos };
+      return { ...state, todos: removeTodo(payload, state.todos)};
     default:
       return { ...state };
   }
@@ -28,6 +30,21 @@ const loadTodos = (): IState => {
   const nextId = todos.length ? Math.max(...todos.map((todo: ITodo) => todo.id)) + 1 : 0
   return { todos,  nextId: nextId};
 }
+
+const updateTodos = (payload: { id: number }, prevTodos: ITodos) => {
+  const { id, ...rest } = payload;
+  return prevTodos.map((todo) => {
+    if (todo.id !== id) return todo;
+    return {
+      ...todo,
+      ...rest,
+      updatedAt: new Date(),
+    }
+  })
+}
+
+const removeTodo = (id: number, prevTodos: ITodos) => 
+  prevTodos.filter((todo: ITodo) => todo.id !== id);
 
 const createTodo = (nextId: number, createTodoDto: CreateTodoDto): ITodo => {
   const { text, due, category } = createTodoDto;
