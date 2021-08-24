a19574867d5e5cf71d4af96936c7e67c16391049
sanam <simian114@gmail.com>
2021-08-24 18:17:27 +0900

    [#15] feat: todo 관련 types 정의

---
+++ src/types/todo.ts
@@ -0,0 +1,34 @@
+export interface ITodo {
+  id: number;
+  text: string;
+  status: TStatus;
+  createdAt: Date | null;
+  updatedAt: Date | null;
+  due: Date | null;
+  category: TCategory;
+  priority: TPriority;
+}
+
+export type ITodos = ITodo[];
+
+export enum TStatus {
+  NOT_STARTED = '시작안함',
+  ONGOING = '진행중',
+  FINISHED = '완료',
+}
+
+export enum TCategory {
+  DEFAULT = '선택',
+  WORK = '업무',
+  STUDY = '공부',
+  LIVING = '생활',
+  EXERCISE = '운동',
+  ETC = '기타',
+}
+
+export enum TPriority {
+  DEFAULT = '선택',
+  HIGH = '상',
+  MIDDLE = '중',
+  LOW = '하',
+}
