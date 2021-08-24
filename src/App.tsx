afc6667b5e7c5605cdebc1e3907ee9cc4b552422
irisdew <ycleardew@gmail.com>
2021-08-25 02:29:08 +0900

    [#3] feat: TodoInput 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
-import { create } from 'context/todoContext/actionCreators';
-import { useTodosDispatch } from 'context/todoContext/TodoContext';
 import React from 'react';
+import TodoCreate from 'components/TodoCreate/TodoCreate';
 
-import { CreateTodoDto, TCategory } from './types';
-
-const App: React.FC<any> = () => {
-  const dispatch = useTodosDispatch();
-
-  const handleClick = () => {
-    const temp: CreateTodoDto = { text: 'test입니다.', due: new Date(), category: TCategory.STUDY };
-    dispatch(create(temp));
-  };
+const App: React.FC = () => {
   return (
     <div className="App">
-      <button onClick={handleClick}>zz</button>
+      <TodoCreate />
     </div>
   );
 };
--- src/components/TodoCreate/TodoCreate.tsx
+++ src/components/TodoCreate/TodoCreate.tsx
@@ -3,9 +3,13 @@ import styled from 'styled-components';
 import { ReactComponent as CirclePlus } from 'assets/svg/circle-plus.svg';
 import { ReactComponent as Calender } from 'assets/svg/calendar.svg';
 import { ReactComponent as DownArraow } from 'assets/svg/down-arrow.svg';
+import { create } from 'context/todoContext/actionCreators';
+import { useTodosDispatch } from 'context/todoContext/TodoContext';
+import { CreateTodoDto, TCategory } from 'types';
 
 const TodoCreate: React.FC = () => {
   const [todoText, setTodoText] = useState<string>('');
+  const dispatch = useTodosDispatch();
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setTodoText(e.target.value);
@@ -13,12 +17,14 @@ const TodoCreate: React.FC = () => {
 
   const handleSubmit = (e: FormEvent<HTMLButtonElement>) => {
     e.preventDefault();
+    const temp: CreateTodoDto = { text: todoText, due: new Date(), category: TCategory.STUDY };
+    dispatch(create(temp));
   };
 
   return (
     <Form>
       <InputContainer>
-        <input
+        <TodoInput
           type="text"
           placeholder="할 일을 입력해주세요."
           value={todoText}
@@ -55,12 +61,13 @@ const InputContainer = styled.div`
   border: 1px solid #c2c2c2;
   border-radius: 5px;
   padding: 7px 10px;
-  input {
-    width: 300px;
-    border: none;
-    :focus {
-      outline: none;
-    }
+`;
+
+const TodoInput = styled.input`
+  width: 300px;
+  border: none;
+  :focus {
+    outline: none;
   }
 `;
 
