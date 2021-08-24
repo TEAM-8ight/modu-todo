7cf804b0e945a3688c3c9364d0a863eebfc646a7
irisdew <ycleardew@gmail.com>
2021-08-25 02:01:15 +0900

    [#3] feat: TodoCreate UI 구현

---
+++ src/components/TodoCreate/TodoCreate.tsx
@@ -0,0 +1,89 @@
+import React, { useState, ChangeEvent, FormEvent } from 'react';
+import styled from 'styled-components';
+import { ReactComponent as CirclePlus } from 'assets/svg/circle-plus.svg';
+import { ReactComponent as Calender } from 'assets/svg/calendar.svg';
+import { ReactComponent as DownArraow } from 'assets/svg/down-arrow.svg';
+
+const TodoCreate: React.FC = () => {
+  const [todoText, setTodoText] = useState<string>('');
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setTodoText(e.target.value);
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+  };
+
+  return (
+    <Form>
+      <InputContainer>
+        <input
+          type="text"
+          placeholder="할 일을 입력해주세요."
+          value={todoText}
+          onChange={handleChange}
+        />
+        <Wrapper>
+          <Calender width="20" height="20" />
+        </Wrapper>
+        <Wrapper>
+          <span>카테고리</span>
+          <DownArraow />
+        </Wrapper>
+      </InputContainer>
+      <Button onClick={handleSubmit}>
+        <CirclePlus width="22" height="22" />
+        추가하기
+      </Button>
+    </Form>
+  );
+};
+
+export default TodoCreate;
+
+const Form = styled.form`
+  display: flex;
+  gap: 7px;
+  font-size: 12px;
+`;
+
+const InputContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  border: 1px solid #c2c2c2;
+  border-radius: 5px;
+  padding: 7px 10px;
+  input {
+    width: 300px;
+    border: none;
+    :focus {
+      outline: none;
+    }
+  }
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  height: 30px;
+  padding: 5px;
+  background-color: ${(props) => props.theme.color.lightGray};
+  border-radius: 5px;
+  text-align: center;
+`;
+
+const Button = styled.button`
+  width: 110px;
+  padding: 7px;
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  justify-content: center;
+  background-color: ${(props) => props.theme.color.darkGray};
+  color: white;
+  font-size: 12px;
+  border-radius: 5px;
+`;
