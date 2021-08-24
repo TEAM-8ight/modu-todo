52e2378e8d864a0c1330be6c6035ed59d69c035d
sanam <simian114@gmail.com>
2021-08-25 00:41:08 +0900

    [#17] chore: dropdown 사용 예 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
-import { create } from 'context/todoContext/actionCreators';
-import { useTodosDispatch } from 'context/todoContext/TodoContext';
+import DropdownTemp from 'components/temp/DropdownTemp';
 import React from 'react';
 
-import { CreateTodoDto, TCategory } from './types';
-
 const App: React.FC<any> = () => {
-  const dispatch = useTodosDispatch();
-
-  const handleClick = () => {
-    const temp: CreateTodoDto = { text: 'test입니다.', due: new Date(), category: TCategory.STUDY };
-    dispatch(create(temp));
-  };
   return (
     <div className="App">
-      <button onClick={handleClick}>zz</button>
+      <DropdownTemp />
     </div>
   );
 };
