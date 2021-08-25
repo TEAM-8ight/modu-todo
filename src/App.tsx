24e0ae4e4a931c06fc914bfbad9827b69fcf0d4a
daeun-react <smile_daeun@naver.com>
2021-08-25 10:52:04 +0900

    [#8] feat : App.tsx에 필터링 컴포넌트 사용 예시 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
+import React from 'react';
+import styled from 'styled-components';
 import { create } from 'context/todoContext/actionCreators';
 import { useTodosDispatch } from 'context/todoContext/TodoContext';
-import React from 'react';
-
+import TodoFilter from 'components/TodoFilter/TodoFilter';
 import { CreateTodoDto, TCategory } from './types';
 
 const App: React.FC<any> = () => {
@@ -13,9 +14,18 @@ const App: React.FC<any> = () => {
   };
   return (
     <div className="App">
+      <Filter>
+        <TodoFilter type="category" />
+        <TodoFilter type="priority" />
+      </Filter>
       <button onClick={handleClick}>zz</button>
     </div>
   );
 };
+
+const Filter = styled.div`
+  display: flex;
+  padding: 20px;
+`;
 
 export default App;
